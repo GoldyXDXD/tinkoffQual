f7920b13c42677babe79ba76e133a13bb8a3609d
GoldyXDXD <62290862+GoldyXDXD@users.noreply.github.com>
2021-09-05 17:08:42 +0300

    task5 is solved (almost)
    Пока не подобрано решение для уравнений 5-й степени.
    P.S. Задание на отборах было легче - там давались уравнения с единственным решением. Данная же программа создана для уравнений с несколькими корнями (действительными).

---
+++ task5/Program.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int degree = in.nextInt();
+        double[] a = new double[degree + 1];
+        double[] result = new double[degree];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = in.nextFloat();
+        }
+        switch (degree) {
+            case 1:
+                result = solveForFirstPower(a);
+                break;
+            case 3:
+                result = solveForThirdPower(a);
+                break;
+            case 5:
+                result = solveForFifthPower(a);
+                break;
+        }
+        System.out.println(result);
+    }
+    public static double[] solveForFirstPower(double[] floats) {
+        double[] result = new double[1];
+        result[0] =  floats[0] / floats[1];
+        return result;
+    }
+    public static double[] solveForThirdPower(double[] floats) { // Тригонометрическая формула Виета
+        double[] result = new double[3];
+        if (floats[3] != 1) {
+            for(int i = 0; i < floats.length; i++) {
+                floats[i] /= floats[3];
+            }
+        }
+        double q = (floats[2] * floats[2] - 3 * floats[1]) / 9;
+        double r = (2 * floats[2] * floats[2] * floats[2] - 9 * floats[1] * floats[2] + 27 * floats[0]) / 54;
+        double fi = Math.acos(r / Math.sqrt(q * q * q));
+        result[0] = -2 * Math.sqrt(q) * Math.cos(fi) - floats[2] / 3;
+        result[1] = -2 * Math.sqrt(q) * Math.cos(fi + 2 * Math.PI / 3) - floats[2] / 3;
+        result[1] = -2 * Math.sqrt(q) * Math.cos(fi - 2 * Math.PI / 3) - floats[2] / 3;
+        return result;
+    }
+    public static double[] solveForFifthPower(double[] floats) {
+        double[] result = new double[5];
+        // пока не подобрал решение
+        return result;
+    }
+}
